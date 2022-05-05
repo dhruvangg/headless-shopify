2b9c952d4748d172e13505162bf13dfc541a2a7b
champdecay <dhruvangg@gmail.com>
2022-05-05 13:06:21 +0530

    Final

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import { Routes, Route, HashRouter } from "react-router-dom";
+import { Routes, Route, HashRouter, Navigate } from "react-router-dom";
 import Header from 'Components/Header'
 import { app } from "firebase.js";
 import { Home, Products, Signup, Login, Profile } from 'Pages'
 import { PrivateRoute, AccessRoute } from "Routes";
+import ErrorBoundary from "Components/ErrorBoundary";
 
 export default function App() {
   return (
     <HashRouter>
-      <main className="min-h-screen bg-white dark:bg-gray-900 dark:text-white">
-        <Header />
-        <Routes>
-          <Route path="" element={<Home />} />
-          <Route path="/product" element={<PrivateRoute><Products /></PrivateRoute>} />
-          <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
-          <Route path="/signup" element={<AccessRoute><Signup /></AccessRoute>} />
-          <Route path="/login" element={<AccessRoute><Login /></AccessRoute>} />
-        </Routes>
-      </main>
+      <ErrorBoundary>
+        <main className="min-h-screen bg-white dark:bg-gray-900 dark:text-white">
+          <Header />
+          <Routes>
+            <Route path="" element={<Navigate to="product" />} />
+            <Route path="/product" element={<Products />} />
+            {/* <Route path="/product" element={<PrivateRoute><Products /></PrivateRoute>} />
+            <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
+            <Route path="/signup" element={<AccessRoute><Signup /></AccessRoute>} />
+            <Route path="/login" element={<AccessRoute><Login /></AccessRoute>} /> */}
+          </Routes>
+        </main>
+      </ErrorBoundary>
     </HashRouter>
   )
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,11 +3,14 @@ import { useState, useEffect, useContext } from 'react'
 import Client from 'shopify-buy';
 import { ThemeContext } from "Context/ThemeContext";
 import { StoreContext } from "Context/StoreContext";
+import { useAuth } from "Context/AuthContext"
 
 export default function Header() {
     const [showCart, setShowCart] = useState(false)
     const { theme, setTheme } = useContext(ThemeContext);
     const { cart, addQuantity, subQuantity, removeItem } = useContext(StoreContext)
+    const { user } = useAuth()
+
     const toggleTheme = () => {
         setTheme(theme === "light" ? "dark" : "light")
     }
@@ -40,7 +43,8 @@ export default function Header() {
             }, [])
 
             client.checkout.addLineItems(checkoutId, lineItemsToAdd).then((checkout) => {
-                window.open = checkout.webUrl
+                console.log(checkout.webUrl);
+                window.open(checkout.webUrl);
             }).catch((err) => {
                 console.log(err)
             })
@@ -48,14 +52,14 @@ export default function Header() {
             console.log(err)
         })
     }
+
     return (
         <header
             className="bg-white dark:bg-gray-900 border-b-2 border-indigo-500 p-2 sticky top-0 w-full flex flex-col justify-center gap-4 p-4">
             <div className="container mx-auto flex justify-between items-center">
-                <h2 className="text-xl font-bold text-indigo-700 dark:text-indigo-500"><Link to="">Shopify</Link></h2>
+                <h2 className="text-xl font-bold text-indigo-700 dark:text-indigo-500"><Link to="">HeadlessShopify</Link></h2>
                 <ul className="flex">
-                    <li className="mx-4 font-semibold"><Link to="/product" className="dark:text-white">Product</Link></li>
-                    <li className="mx-4 font-semibold"><Link to="/orders" className="dark:text-white">Orders</Link></li>
+                    {/* <li className="mx-4 font-semibold"><Link to="/product" className="dark:text-white">Product</Link></li> */}
                 </ul>
                 <div className="flex">
                     <button className="mr-4" onClick={toggleTheme}>
@@ -76,57 +80,57 @@ export default function Header() {
                 </div>
             </div>
             <aside className={`w-96 fixed top-0 right-0 bg-white dark:bg-gray-800 dark:text-white min-h-screen shadow-2xl p-8 transition-all duration-300 overflow-auto h-screen ${showCart ? 'translate-x-0' : 'translate-x-full'}`}>
-                <button className="fixed top-5 right-5" onClick={() => setShowCart(false)}>
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                        <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
-                    </svg>
-                </button>
-
-                {cart.length > 0 ? <div className="flex flex-col justify-between">
-                    <h3 className="font-bold">Cart</h3>
-                    <ul className="flex flex-col">
-                        {cart.map((item) => {
-                            return <li className="flex py-6" key={item.id}>
-                                <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-                                    <img src={item.image} alt={item.title} className="h-full w-full object-cover object-center" />
-                                </div>
-                                <div className="ml-4 flex flex-1 flex-col">
-                                    <div>
-                                        <div className="flex justify-between text-base font-medium text-gray-900 dark:text-white">
-                                            <h3><a href="#">{item.title}</a></h3>
-                                            <p className="ml-4">₹{parseFloat(item.price) * parseInt(item.quantity)}</p>
-                                        </div>
-                                        <p className="mt-1 text-sm text-gray-500 dark:text-gray-300">Unit Price: ₹{parseFloat(item.price)}</p>
+                <div className="flex flex-col justify-between">
+                    <div className="flex justify-between">
+                        <h3 className="font-bold">Cart</h3>
+                        <button className="" onClick={() => setShowCart(false)}>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
+                            </svg>
+                        </button>
+                    </div>
+                    {cart.length > 0 ? <>
+                        <ul className="flex flex-col">
+                            {cart.map((item) => {
+                                return <li className="flex py-6" key={item.id}>
+                                    <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+                                        <img src={item.image} alt={item.title} className="h-full w-full object-cover object-center" />
                                     </div>
-                                    <div className="flex flex-1 items-end justify-between text-sm">
-                                        <div className="text-gray-500 dark:text-gray-300 flex items-center">Qty
-                                            <div className="flex px-4 items-center">
-                                                <button onClick={() => subQuantity(item.id)}>
-                                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                                                        <path fillRule="evenodd" d="M5 10a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1z" clipRule="evenodd" />
-                                                    </svg>
-                                                </button>
-                                                <span className="m-2 ">{item.quantity}</span>
-                                                <button onClick={() => addQuantity(item.id)}>
-                                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                                                        <path fillRule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clipRule="evenodd" />
-                                                    </svg>
-                                                </button>
+                                    <div className="ml-4 flex flex-1 flex-col">
+                                        <div>
+                                            <div className="flex justify-between text-base font-medium text-gray-900 dark:text-white">
+                                                <h3><a href="#">{item.title}</a></h3>
+                                                <p className="ml-4">₹{parseFloat(item.price) * parseInt(item.quantity)}</p>
+                                            </div>
+                                            <p className="mt-1 text-sm text-gray-500 dark:text-gray-300">Unit Price: ₹{parseFloat(item.price)}</p>
+                                        </div>
+                                        <div className="flex flex-1 items-end justify-between text-sm">
+                                            <div className="text-gray-500 dark:text-gray-300 flex items-center">Qty
+                                                <div className="flex px-4 items-center">
+                                                    <button onClick={() => subQuantity(item.id)}>
+                                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                                            <path fillRule="evenodd" d="M5 10a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1z" clipRule="evenodd" />
+                                                        </svg>
+                                                    </button>
+                                                    <span className="m-2 ">{item.quantity}</span>
+                                                    <button onClick={() => addQuantity(item.id)}>
+                                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                                            <path fillRule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clipRule="evenodd" />
+                                                        </svg>
+                                                    </button>
+                                                </div>
+                                            </div>
+                                            <div className="flex">
+                                                <button type="button" className="font-medium text-indigo-600 hover:text-indigo-500" onClick={() => removeItem(item.id)}>Remove</button>
                                             </div>
                                         </div>
-                                        <div className="flex">
-                                            <button type="button" className="font-medium text-indigo-600 hover:text-indigo-500" onClick={() => removeItem(item.id)}>Remove</button>
-                                        </div>
                                     </div>
-                                </div>
-                            </li>
-                        })}
-                    </ul>
-                    <button type="button" className="font-semibold bg-indigo-700 dark:bg-gray-800 py-2 px-2 text-gray-300 w-full rounded-md" onClick={handleCheckout}>Checkout</button>
-                </div> : <div className="flex flex-col justify-between">
-                    <h3 className="font-bold">Cart</h3>
-                    <p>Your cart is empty</p>
-                </div>}
+                                </li>
+                            })}
+                        </ul>
+                        <button type="button" className="font-semibold bg-indigo-700 dark:bg-gray-800 py-2 px-2 text-gray-300 w-full rounded-md" onClick={handleCheckout}>Checkout</button>
+                    </> : <p>Your cart is empty</p>}
+                </div>
             </aside>
         </header >
     )
